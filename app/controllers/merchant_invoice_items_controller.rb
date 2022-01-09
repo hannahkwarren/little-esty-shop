530670b3f98e6c6b32f718f635a8477bab413012
Darren Kulback <dkulback@gmail.com>
2022-01-09 09:07:12 -0700

    remove pry

--- app/controllers/merchant_invoice_items_controller.rb
+++ app/controllers/merchant_invoice_items_controller.rb
@@ -1,7 +1,6 @@
 class MerchantInvoiceItemsController < ApplicationController
 
   def update
-    require "pry"; binding.pry
     invoice_item = InvoiceItem.find(params[:id])
     invoice_item.update(invoice_item_params)
     redirect_to merchant_invoice_path(invoice_item.item.merchant, invoice_item.invoice_id)
