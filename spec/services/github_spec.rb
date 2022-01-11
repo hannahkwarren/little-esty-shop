f805e673cf04f17f63c7fdceb6b21708fbbca7a7
Eldridge Turambi <eturambi95@gmail.com>
2022-01-11 15:39:13 -0700

    Updated user_names_and_commits method in services

--- spec/services/github_spec.rb
+++ spec/services/github_spec.rb
@@ -4,14 +4,25 @@ RSpec.describe GithubService do
   let(:github_api) {GithubService.new}
   it 'pases the api' do
     actual = github_api.repo_name
-    expected = "little esty shop"
+    expected = "little-esty-shop"
     expect(actual).to eq(expected)
   end
-  describe '#user_names' do
+  describe '#user_names_and_commits' do
     it 'returns the github usernames for the repo' do
-      actual = github_api.user_names
-      expected = "Eldridge-Turambi Malllll12 dkulback hannahkwarren"
+      actual = github_api.user_names_and_commits
+      expected = {"Eldridge-Turambi" => 35,
+                  "Malllll12" => 52,
+                  "dkulback" => 51,
+                  "hannahkwarren" => 70}
       expect(actual).to eq(expected)
     end
   end
+
+  # describe '#user_commits' do
+  #   it 'returns github commits for each user' do
+  #     actual = github_api.user_commits
+  #     expected = "Eldridge-Turambi Malllll12 dkulback hannahkwarren"
+  #     expect(actual).to eq(expected)
+  #   end
+  # end
 end
