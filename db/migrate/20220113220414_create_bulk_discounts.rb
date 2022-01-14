c7bb8eace92437d5a802ee3f41ec95622bbe168f
Darren Kulback <dkulback@gmail.com>
2022-01-13 20:03:34 -0700

    Add bulk_discounts

---
+++ db/migrate/20220113220414_create_bulk_discounts.rb
@@ -0,0 +1,12 @@
+class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bulk_discounts do |t|
+      t.references :merchant, foreign_key: true
+      t.string :title
+      t.integer :qty_threshold
+      t.decimal :percentage
+
+      t.timestamps
+    end
+  end
+end
