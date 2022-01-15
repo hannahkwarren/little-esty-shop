d2a28bfd8625d4bdf4cceb61e3907822d2f5a5fa
Hannah <hannahkwarren@gmail.com>
2022-01-15 17:00:20 -0500

    Revert "Crud create"

--- app/models/bulk_discount.rb
+++
@@ -1,6 +0,0 @@
-class BulkDiscount < ApplicationRecord
-  validates :name, presence: true
-  validates :threshold, presence: true
-  validates :percentage, presence: true
-  belongs_to :merchant
-end
