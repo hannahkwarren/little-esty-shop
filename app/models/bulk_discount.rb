3ba997fd446daaec081ed13710a7d959695cfc42
Mallory Vining <mallorygallery12@gmail.com>
2022-01-14 19:02:13 -0700

    Add method to fix percent

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,8 @@ class BulkDiscount < ApplicationRecord
   validates :threshold, presence: true
   validates :discount, presence: true
   belongs_to :merchant
+
+  def float_to_percent
+    discount * 100
+  end
 end
--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -7,16 +7,16 @@ RSpec.describe "Bulk Discounts Index page" do
 
     discount_a = merch_1.bulk_discounts.create({name:"Discount A", discount: 0.20, threshold: 10 })
     discount_b = merch_1.bulk_discounts.create({name:"Discount B", discount: 0.30, threshold: 15 })
-    discount_c = merch_2.bulk_discounts.create({name:"Discount C", discount: 0.25, threshold: 20 })
+    discount_c = merch_2.bulk_discounts.create({name:"Discount C", discount: 0.25, threshold: 25 })
 
     visit "/merchants/#{merch_1.id}/bulk_discounts/"
 
     expect(page).to have_content(discount_a.name)
     expect(page).to have_content(discount_b.name)
     expect(page).to_not have_content(discount_c.name)
-    expect(page).to have_content(discount_a.discount)
-    expect(page).to have_content(discount_b.discount)
-    expect(page).to_not have_content(discount_c.discount)
+    expect(page).to have_content("%20.0")
+    expect(page).to have_content("%30.0")
+    expect(page).to_not have_content("%25.0")
     expect(page).to have_content(discount_a.threshold)
     expect(page).to have_content(discount_b.threshold)
     expect(page).to_not have_content(discount_c.threshold)
