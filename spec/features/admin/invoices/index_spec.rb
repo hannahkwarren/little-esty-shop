73ee74d15677a7d5a71368beeec21e9c7bb5acb6
Hannah Warren <hannahkwarren@gmail.com>
2022-01-17 18:02:45 -0500

    Feature: display Total Discounted Revenue on the Admin Invoice show page (including all invoice_items from different merchants)

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin Invoice Index page' do
+
   before(:each) do
     @cust_1 = Customer.create!(first_name:"Hannah", last_name:"Warner")
 
@@ -9,16 +10,15 @@ RSpec.describe 'Admin Invoice Index page' do
     @invoice_3 = Invoice.create!(customer_id:"#{@cust_1.id}", status:1)
   end
 
+  it 'displays a list of all Invoice ids in the system' do
+    visit admin_invoices_path
 
-    it 'displays a list of all Invoice ids in the system' do
-      visit admin_invoices_path
-
-      within ".invoices" do
-        expect(page).to have_content(@invoice_1.id)
-        expect(page).to have_content(@invoice_2.id)
-        expect(page).to have_content(@invoice_3.id)
-      end
+    within ".invoices" do
+      expect(page).to have_content(@invoice_1.id)
+      expect(page).to have_content(@invoice_2.id)
+      expect(page).to have_content(@invoice_3.id)
     end
+  end
 
   it 'links to admin invoice show page' do
     visit admin_invoices_path
--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -14,13 +14,14 @@ RSpec.describe 'Admin Invoice Show Page' do
     @invoice_3 = Invoice.create!(customer_id:"#{@cust_1.id}", status:1)
 
     @invoice_item_1 = InvoiceItem.create!(invoice_id:"#{@invoice_1.id}", item_id:"#{@item_1.id}", status: 2, quantity:1, unit_price:600)
-    @invoice_item_2 = InvoiceItem.create!(invoice_id:"#{@invoice_2.id}", item_id:"#{@item_2.id}", status: 2, quantity:1, unit_price:700)
+    @invoice_item_2 = InvoiceItem.create!(invoice_id:"#{@invoice_2.id}", item_id:"#{@item_2.id}", status: 2, quantity:10, unit_price:700)
     @invoice_item_3 = InvoiceItem.create!(invoice_id:"#{@invoice_3.id}", item_id:"#{@item_2.id}", status: 2, quantity:1, unit_price:700)
+
+    @bulk_discount_1 = BulkDiscount.create(merchant_id: @merch_2, percentage: 10, quantity:10)
   end
 
-
   it 'shows customer and status information for the invoice' do
-    visit "/admin/invoices/#{@invoice_1.id}"
+    visit admin_invoice_path(@invoice_1)
 
     expect(page).to have_content("Invoice ID: #{@invoice_1.id}")
     expect(page).to have_content("Invoice Status: #{@invoice_1.status}")
@@ -30,7 +31,7 @@ RSpec.describe 'Admin Invoice Show Page' do
   end
 
   it 'shows Item information for the the show page' do
-    visit "/admin/invoices/#{@invoice_1.id}"
+    visit admin_invoice_path(@invoice_1)
 
     within ".invoice" do
       expect(page).to have_content(@item_1.name)
@@ -43,7 +44,7 @@ RSpec.describe 'Admin Invoice Show Page' do
   end
 
   it 'shows total revenue for generated from an invoice' do
-    visit "/admin/invoices/#{@invoice_1.id}"
+    visit admin_invoice_path(@invoice_1)
 
     within ".total_revenue" do
     expect(page).to have_content("Total Revenue: #{@invoice_1.total_revenue}")
@@ -51,12 +52,23 @@ RSpec.describe 'Admin Invoice Show Page' do
   end
 
   it 'uses a select field to update invoice status' do
-    visit "/admin/invoices/#{@invoice_1.id}"
+    visit admin_invoice_path(@invoice_1)
 
-      expect(page).to have_content(@invoice_1.status)
-      select 'completed', from: "Status"
-      click_button "Update Status"
-      expect(current_path).to eq(admin_invoices_path(:id))
-      expect(@invoice_1.status).to eq('completed')
+    expect(page).to have_content(@invoice_1.status)
+    select 'completed', from: "Status"
+
+    click_button "Update Status"
+
+    expect(current_path).to eq(admin_invoices_path(:id))
+    expect(@invoice_1.status).to eq('completed')
   end
+
+  it 'has total discounted revenue' do 
+
+    visit admin_invoice_path(@invoice_2.id)
+
+    expect(page).to have_content("Total Revenue: #{@invoice_2.total_revenue}")
+    expect(page).to have_content("Total Discounted Revenue: #{@invoice_2.total_discounted_revenue}")
+  end
+
 end
