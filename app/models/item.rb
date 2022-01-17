c85f0e5da006af0b695192d2313e30bab0aab5d2
Hannah Warren <hannahkwarren@gmail.com>
2022-01-17 14:27:37 -0500

    Makes ActiveRecord queries more readable

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,16 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
+  has_many :bulk_discounts, through: :merchant
   enum status: %i[enabled disabled]
 
   def self.top_five
-    select('items.*, sum(invoice_items.unit_price * invoice_items.quantity) as sum').order(sum: :desc).joins(:transactions, :invoice_items).where(:transactions => {result: 'success'}).group(:id).limit(5)
+    select('items.*, sum(invoice_items.unit_price * invoice_items.quantity) as sum')
+    .order(sum: :desc)
+    .joins(:transactions, :invoice_items)
+    .where(:transactions => {result: 'success'})
+    .group(:id)
+    .limit(5)
   end
 
   def best_date
